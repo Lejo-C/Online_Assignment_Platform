ab41d9268a73eacae7b4b5f088ac051f1e4bbdb6
Lejo-C <lejo3027@gmail.com>
2025-11-02 10:15:51 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,19 @@ const app = express();
 
 app.use(cors({
   origin: 'https://online-assignment-platform.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
 
+app.options('*', cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://online-assignment-platform.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 const server = http.createServer(app);
 
